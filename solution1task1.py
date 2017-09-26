04d3a05722de57dc2747d92d9f1713052f4dd028
aneeshjose <aneeshjose94@gmail.com>
2017-09-26 12:07:57 +0000

    Reinitialised tasks and keys

--- solution1task1.py
+++
@@ -1,5 +0,0 @@
-print("You")
-print("are")
-print("_Super")
-print("_143awesome")
-print("sumONe")
--- solution4task4.py
+++
@@ -1,3 +0,0 @@
-print("""You're Just levelling Up !""")
-print("""I like you're Spirit""")
-print("""Keep up the pace !""")
